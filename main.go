481dcbdb00ac42bf476bcd16430221103ae281e3
Ali Shalabi <ashalabi13@gmail.com>
2020-01-08 12:36:02 -0800

    First commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "flag"
+  // "fmt"
+  // "net/http"
+
+  // "github.com/alishalabi/link_parser"
+)
+
+
+func main() {
+  urlFlag := flag.String("url", "https://makeschool.com", "Website for which you want to build a sitemap")
+  flag.Parse()
+
+
+}
