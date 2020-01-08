9191750421dfae07dc8de3040b399242b6fb05b7
Ali Shalabi <ashalabi13@gmail.com>
2020-01-08 12:52:26 -0800

    Managed to implement GET, print html of website to console

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
   "flag"
   // "fmt"
-  // "net/http"
+  "net/http"
+  "io"
+  "os"
 
   // "github.com/alishalabi/link_parser"
 )
@@ -12,6 +14,21 @@ import (
 func main() {
   urlFlag := flag.String("url", "https://makeschool.com", "Website for which you want to build a sitemap")
   flag.Parse()
+
+  // Initiate GET request
+  resp, err := http.Get(*urlFlag)
+  if err != nil {
+    panic(err)
+  }
+  // Must ALWAYS close response body, or memory leak will occur
+  defer resp.Body.Close() // defer will run when function ends
+
+  io.Copy(os.Stdout, resp.Body)
+
+
+
+
+
 
 
 }
